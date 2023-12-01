dfbc9e475ef1eb809e620b6dc183b97481488500
Robert Walsh <rwalsh@excalibur-solutions.com>
2023-12-01 10:59:36 -0500

    Draw the net on the court

--- court.py
+++ court.py
@@ -18,4 +18,16 @@ class Court:
         pygame.draw.line(self.surface, colors.WHITE, (0, 0), (right, 0))
         pygame.draw.line(self.surface, colors.WHITE, (0, bottom), (right, bottom))
 
+        self.draw_net()
+
         surface.blit(self.surface, (self.x, self.y))
+
+    def draw_net(self):
+        pos = 0
+        center = self.surface.get_rect().centerx
+        pen_down = False
+        while pos < self.surface.get_height():
+            if pen_down:
+                pygame.draw.line(self.surface, colors.WHITE, (center, pos), (center, pos + 10))
+            pos += 10
+            pen_down = not pen_down
