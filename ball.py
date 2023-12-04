4e0e50893f6cb13f87fb1554163649d8f24c37c1
Robert Walsh <rwalsh@excalibur-solutions.com>
2023-12-04 16:40:23 -0500

    Make the ball move

--- ball.py
+++ ball.py
@@ -1,9 +1,14 @@
 import pygame
+import random
+import math
 import colors
 
 class Ball:
 
     SIZE = 5
+    INITIAL_SPEED = 5
+    MAX_ANGLE = 60
+    MIN_ANGLE = -MAX_ANGLE
 
     def __init__(self, court):
         self.size = Ball.SIZE
@@ -13,9 +18,21 @@ class Ball:
 
     def initialize_ball(self):
         self.position = self.initial_position
+        angle = math.radians(random.randint(Ball.MIN_ANGLE, Ball.MAX_ANGLE) + random.choice([0, 180]))
+        self.delta_x = math.cos(angle)
+        self.delta_y = math.sin(angle)
+        self.speed = Ball.INITIAL_SPEED
 
     def update(self, bounds):
-        pass
+        new_x = self.get_new_x()
+        new_y = self.get_new_y()
+        self.position = (new_x, new_y)
+
+    def get_new_x(self):
+        return self.position[0] + self.speed * self.delta_x
+    
+    def get_new_y(self):
+        return self.position[1] + self.speed * self.delta_y
 
     def draw(self, surface):
         pygame.draw.circle(surface, colors.WHITE, self.position, self.size)
